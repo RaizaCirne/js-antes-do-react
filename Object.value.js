b09b1059e8c2b7dc355023ba63a0286f3dcc0d02
Raiza Cirne <raizacirne@gmail.com>
2024-02-18 15:47:58 -0300

    Feat: add metodo Object.values()

---
+++ Object.value.js
@@ -0,0 +1,15 @@
+// Object.values → método pra retornar todos os valores do objeto ao invés das chaves
+const user = {
+  name: 'Diego',
+  idade: 27,
+  address: {
+    street: 'Rua teste',
+    number: 176,
+  },
+}
+
+// Retorna Diego,27,[object Object]
+document.body.innerText = Object.values(user);
+
+// Retonar object Object porque não conseguiu entender como ele printa esse valor em tela.
+// Tática pra mostrar estruturas de dados mais complexas em tela é usar o JASON.stringfy().
