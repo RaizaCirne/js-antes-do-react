15d6952bb9ebbf751539086fd74ecd2e49b67f7a
Raiza Cirne <raizacirne@gmail.com>
2024-02-18 15:37:04 -0300

    Feat: add expressao in

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-// Nullish Coalescing Operator 
-const idade = null;
-document.body.innerText = 'Sua idade é: ' + (idade ?? 'Não informado') ; 
+// Objetos
+
+const user = {
+  name: 'Diego',
+  idade: 27,
+  // o endereço é um objeto aninhado, ou seja, um objeto dentro do outro
+  address: {
+    street: 'Rua teste',
+    number: 176,
+  },
+}
+
+document.body.innerText = ('name' in user);
