d6b1ecb18622f778487d43d30267a3ac65d475ec
Raiza Cirne <raizacirne@gmail.com>
2024-02-19 08:28:52 -0300

    Fix: corrigir erro sintaxe

--- rest-operator.js
+++ rest-operator.js
@@ -12,5 +12,5 @@ const user = {
 // Se eu desestruturo a propriedade name do usuário e quero pegar o resto: 
 // usar ...rest dentro da desestruturação
 
-const { name, idade ...rest } = user; // retorna o resto das informações que sobraram a partir do name e da idade.
+const { name, idade, ...rest } = user; // retorna o resto das informações que sobraram a partir do name e da idade.
 document.body.innerText = JSON.stringify(rest); // retorna um objeto contendo todas as propriedades do objeto, menos a propriedade name e idade. Ou seja, é o resto das propriedades do usuário, menos as que eu ja tirei.
