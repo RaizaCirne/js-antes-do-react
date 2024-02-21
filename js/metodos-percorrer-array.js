d7f26cb9ed3981d65f2b3928434de0be56b94218
Raiza Cirne <raizacirne@gmail.com>
2024-02-21 08:33:43 -0300

    Feat: Percorrer array com for

---
+++ js/metodos-percorrer-array.js
@@ -0,0 +1,9 @@
+const array = [1, 2, 3, 4, 5];
+
+// for() -> uma das formas de percorrer um array 
+for(const i of array) {
+  // document.body.innerText += i; -> para mostrar na tela
+  console.log(i);
+}
+
+// forEach() -> outra forma de percorrer um array 
