1974f8ca233f10414f984d5c073ffad15e90b967
Raiza Cirne <raizacirne@gmail.com>
2024-02-21 09:11:58 -0300

    Feat: add método map()

--- js/forEach().js
+++ js/forEach().js
@@ -2,7 +2,20 @@
 
 const array = [1, 2, 3, 4, 5]; 
  
+/*
 array.forEach(item => {
   // document.body.innerText += item; -> para mostrar na tela
   console.log(item);
 })
+*/
+
+// Se eu quero percorrer um array e retornar algo a partir dele, o forEach() não é bom. 
+// Ex: Criar um novo array e percorrer ele e multiplicar todos os números do array por 2
+
+const novoArray = []; 
+
+array.forEach(item => {
+  novoArray.push(item * 2);
+});
+
+console.log(novoArray);
--- js/metodos-percorrer-array.js
+++ js/metodos-percorrer-array.js
@@ -11,3 +11,7 @@ array.forEach(item => {
   // document.body.innerText += item; -> para mostrar na tela
   console.log(item);
 })
+
+// map() -> diferença map() X forEach -> Com o map() é possível fazer um retorno de dentro do map()
+// Se eu quero percorrer um array e retornar algo a partir dele, o forEach() não é bom. 
+// Ex: Criar um novo array e percorrer ele e multiplicar todos os números do array por 2
